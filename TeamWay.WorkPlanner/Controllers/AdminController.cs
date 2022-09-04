af3d69aaa85e39dbd7cbf9a6385c48be20d98c7d
kuldeep.singh <kuldeep.singh@unitalks.co>
2022-09-04 15:37:23 +0530

    Initial code push
    Initial code push

---
+++ TeamWay.WorkPlanner/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamWay.WorkPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+    }
+}
